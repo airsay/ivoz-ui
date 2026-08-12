3e9d8e430f25102a296d5a3eb0d0a0b57fdfc08f
airsay <airsay@users.noreply.github.com>
2026-08-12 10:00:24 -0400

    Updated Footer to NorthBay

--- library/src/components/layout/Footer.tsx
+++ library/src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { Typography } from '@mui/material';
 export default function Footer(): JSX.Element {
   return (
     <Typography variant='body2' color='textSecondary' align='center'>
-      Irontec &nbsp;
+      NorthBay &nbsp;
       {new Date().getFullYear()}
     </Typography>
   );
